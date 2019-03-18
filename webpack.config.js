76a5fcfd2280e1312ce9e60b1487b704bee182ec
Sean Baskin <seanbaskin@gmail.com>
2019-03-18 19:13:42 -0400

    Configured HTML webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,57 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
+/**
+ *
+ */
 module.exports = {
-  mode: 'development',
   entry: {
     app: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    publicPath: 'http://localhost:8080/dist',
-    hot: true
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'baskinomics.github.io'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader"
-        },
-        options: { presets: ['@babel/env'] }
+        use: [{
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/env']
+          }
+        }],
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: "html-loader" }]
       },
       {
         test: /\.css$/,
         use: [
-          'style-loader',
-          'css-loader'
+          { loader: "style-loader" },
+          { loader: "css-loader" }
         ]
       }
     ]
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] }
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    publicPath: 'http://localhost:8080/dist',
+    hot: true
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  }
 };
