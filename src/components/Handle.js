c21c31dac7239e95e3db760870911d67a92b304c
Sean Baskin <seanbaskin@gmail.com>
2019-03-19 16:00:48 -0400

    Created React components from prior assets

---
+++ src/components/Handle.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../styles/Handle.css";
+
+class Handle extends Component {
+  render() {
+    return (
+      <div className="Handle">
+        <h1>&#36; baskinomics</h1>
+      </div>
+    );
+  }
+}
+
+export default Handle;
